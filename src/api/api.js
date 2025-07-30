0c7ff4957678c04cc870fe215bae4bd3fcc799c5
Andy Christophe <andy.christ971@gmail.com>
2025-07-30 16:01:26 +0200

    fixed base url axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "lapince-back-production.up.railway.app",
+  baseURL: "https://lapince-back-production.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
